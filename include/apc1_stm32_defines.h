4d4f23a252cc7802ae8a342ce5b4df417c237555
Philipp Tillmann <p.tillmann@engiro.de>
2025-04-21 08:54:21 +0200

    updated defines to match apc_stm_32_io_uart

--- include/apc1_stm32_defines.h
+++ include/apc1_stm32_defines.h
@@ -1,7 +1,9 @@
 #ifndef APC1_STM32_DEFINES_H_
 #define APC1_STM32_DEFINES_H_
 
-#define APC1_CMD_READ_MEASUREMENT  0x04
-#define APC1_FRAME_LENGTH          64
+#define APC1_CMD_SIZE 7     // The size of the command (7 bytes)
+#define APC1_RESPONSE_LENGTH 64  // Expected response length (64 bytes)
+
+#define APC1_UART_TIMEOUT_MS  500U
 
 #endif
